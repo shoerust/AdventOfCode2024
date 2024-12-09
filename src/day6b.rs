c01071420be07c46d538677e06c8a096d87a8d9e
William Southers <william@southers.id.au>
2024-12-09 19:56:03 +1100

    chore(main): day6b

--- src/day6b.rs
+++ src/day6b.rs
@@ -1,6 +1,8 @@
 use std::fs::File;
 use std::io::{BufRead, BufReader};
-use std::collections::{HashMap};
+use std::collections::HashMap;
+
+// This is a brute force solution - it's not pretty!
 
 pub fn solve() {
     let filename = "./inputs/day6.txt";
@@ -41,97 +43,90 @@ pub fn solve() {
     }
 
     let mut total = 0;
+    let mut path: Vec<(isize, isize)> = Vec::new();
     let mut exited = false;
     while !exited {
-        if guard_pos.0 == (map.len() as isize - 1) || guard_pos.1 == (map[guard_pos.0 as usize].len() as isize - 1) ||
-            guard_pos.0 == 0 || guard_pos.1 == 0 {
+        if guard_pos.0 == (map.len() as isize - 1) || guard_pos.1 == (map[guard_pos.0 as usize].len() as isize - 1) {
             mark_seen(guard_pos, &mut map);
+            path.push((guard_pos.0, guard_pos.1));
             exited = true;
         } else {
-            let mut turned = false;
             let next_pos = (guard_pos.0 + directions[&guard].0, guard_pos.1 + directions[&guard].1);
-            if next_pos.0 > -1 && next_pos.1 > -1 {
-                if map[next_pos.0 as usize][next_pos.1 as usize]  == '#' ||
-                    map[next_pos.0 as usize][next_pos.1 as usize]  == '$' {
-                    map[next_pos.0 as usize][next_pos.1 as usize] = '$';
-                    guard = turns[&guard];
-                    turned = true;
-                } else {
-                    mark_seen(guard_pos, &mut map);
-                    if guard == '^' {
-                        for i in guard_pos.1..map[guard_pos.0 as usize].len() as isize {
-                            if map[guard_pos.0 as usize][i as usize] == '#' {
-                                break;
-                            }
-                            if map[guard_pos.0 as usize][i as usize] == '$' {
-                                map[next_pos.0 as usize][next_pos.1 as usize] = 'O';
-                                break;
-                            }
-                        }
-                    }
-                    if guard == 'v' {
-                        for i in (0..guard_pos.1).rev() {
-                            if map[guard_pos.0 as usize][i as usize] == '#' {
-                                break;
-                            }
-                            if map[guard_pos.0 as usize][i as usize] == '$' {
-                                map[next_pos.0 as usize][next_pos.1 as usize] = 'O';
-                                break;
-                            }
-                        }
-                    }
-                    if guard == '>' {
-                        for i in guard_pos.0..map.len() as isize {
-                            if map[i as usize][guard_pos.1 as usize] == '#' {
-                                break;
-                            }
-                            if map[i as usize][guard_pos.1 as usize] == '$' {
-                                map[next_pos.0 as usize][next_pos.1 as usize] = 'O';
-                                break;
-                            }
-                        }
-                    }
-                    if guard == '<' {
-                        for i in (0..guard_pos.0).rev() {
-                            if map[i as usize][guard_pos.1 as usize] == '#' {
-                                break;
-                            }
-                            if map[i as usize][guard_pos.1 as usize] == '$' {
-                                map[next_pos.0 as usize][next_pos.1 as usize] = 'O';
-                                break;
-                            }
-                        }
-                    }
-                }
-            }
-            if !turned {
+            if map[next_pos.0 as usize][next_pos.1 as usize]  == '#' {
+                guard = turns[&guard];
+            } else {
+                mark_seen(guard_pos, &mut map);
                 guard_pos = next_pos;
-            }
-        }
-
-    }
-
-    for i in 0..map.len() {
-        for j in 0..map[i].len() {
-            if map[i][j] == 'O' {
-                total += 1
+                path.push((guard_pos.0, guard_pos.1));
             }
         }
     }
 
+    //find guard
+    for (row, row_chars) in map.iter().enumerate() {
+        for (col, _) in row_chars.iter().enumerate() {
+            if directions.contains_key(&map[row][col]) {
+                guard_pos = (row as isize, col as isize);
+                guard = *row_chars.get(col).unwrap();
+                break;
+            }
+        }
+    }
 
-    print_map(&map);
+    print_map(map.clone());
+
+    for row in 0..map.len() {
+        for col in 0..map[row].len() {
+            if map[row][col] == 'X' {
+                total += evaluate_obstacle(map.clone(), &directions, &turns, guard.clone(), guard_pos.clone(), (row as isize, col as isize));
+            }
+        }
+    }
+
     println!("Total: {}", total);
 }
 
-fn print_map(map: &Vec<Vec<char>>) {
-    for row in map {
+fn evaluate_obstacle(mut map: Vec<Vec<char>>, directions: &HashMap<char, (isize, isize)>, turns: &HashMap<char, char>, mut guard: char, mut guard_pos: (isize, isize), obstacle_pos: (isize, isize)) -> i32 {
+    let mut exited = false;
+    map[obstacle_pos.0 as usize][obstacle_pos.1 as usize] = '#';
+
+    let mut seen_map: HashMap<(isize, isize), Vec<char>> = HashMap::new();
+
+    while !exited {
+        if (guard_pos.0 == (map.len() as isize - 1)) ||
+            (guard_pos.1 == (map[guard_pos.0 as usize].len() as isize - 1)) ||
+                (guard_pos.0 == 0) ||
+                (guard_pos.1 == 0) {
+            exited = true;
+        } else {
+            let next_pos = (guard_pos.0 + directions[&guard].0, guard_pos.1 + directions[&guard].1);
+            if seen_map.contains_key(&(guard_pos.0, guard_pos.1)) {
+                if seen_map.get(&(guard_pos.0, guard_pos.1)).unwrap().contains(&guard) {
+                    return 1
+                }
+                seen_map.get_mut(&(guard_pos.0.clone(), guard_pos.1.clone())).unwrap().push(guard.clone());
+            } else {
+                seen_map.insert((guard_pos.0, guard_pos.1), vec![guard.clone()]);
+            }
+
+            if map[next_pos.0 as usize][next_pos.1 as usize]  == '#' {
+                guard = turns[&guard];
+            } else {
+                guard_pos = next_pos;
+            }
+        }
+    }
+    0
+}
+
+fn print_map(map: Vec<Vec<char>>) {
+    for row in &map {
         for &cell in row {
             print!("{}", cell);
         }
         println!();
     }
-    println!("-------")
+    println!("-------");
 }
 
 fn mark_seen(guard_pos: (isize, isize), map: &mut Vec<Vec<char>>) {
